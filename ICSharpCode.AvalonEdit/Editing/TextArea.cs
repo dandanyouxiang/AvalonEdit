475bc462f0001bdf20bb8e11c6d4ece756f75fdd
de4dot <de4dot@gmail.com>
2015-05-26 18:36:01 +0200

    Comment out Debug.WriteLine() messages that fill output window with unneeded messages

--- ICSharpCode.AvalonEdit/Editing/TextArea.cs
+++ ICSharpCode.AvalonEdit/Editing/TextArea.cs
@@ -536,7 +536,7 @@ namespace ICSharpCode.AvalonEdit.Editing
 			ensureSelectionValidRequested = false;
 			if (allowCaretOutsideSelection == 0) {
 				if (!selection.IsEmpty && !selection.Contains(caret.Offset)) {
-					Debug.WriteLine("Resetting selection because caret is outside");
+					//Debug.WriteLine("Resetting selection because caret is outside");
 					this.ClearSelection();
 				}
 			}
